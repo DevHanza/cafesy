6e10505c0093150bde86c4bba0909c9215124ccc
hansanad <hansana123dasanayake@gmail.com>
2023-05-16 22:27:12 +0530

    Sass File Organized a little bit

--- public/js/main.js
+++ public/js/main.js
@@ -17,3 +17,17 @@ function scrollFunction() {
 }
 
 // OnScroll Loading Effect //
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }  
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
