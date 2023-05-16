2259f377b50fb4af688798074d71bc43d0082450
hansanad <hansana123dasanayake@gmail.com>
2023-05-16 21:47:48 +0530

    Site Configured properly with ExpressJS and EJS

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get("/", (req, res)=> {
+    res.render("index");
+});
+
+app.listen(3000, (req, res) => {
+    console.log("Server is up & Running on port 3000.")
+});
