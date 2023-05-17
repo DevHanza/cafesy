d3f05feb2e01320f190c554c0283e6e1ea8c00ff
hansanad <hansana123dasanayake@gmail.com>
2023-05-17 12:04:09 +0530

    Home Page Animation Done

--- public/js/main.js
+++ public/js/main.js
@@ -16,53 +16,6 @@ function scrollFunction() {
 
 // OnScroll Loading Effect //
 
-// const observer = new IntersectionObserver(
-//   (entries) => {
-//     entries.forEach((entry) => {
-//       console.log(entry);
-//       if (entry.isIntersecting && !hasAnimationPlayed(entry.target)) {
-//         entry.target.classList.add(
-//           "slide-up-show",
-//           "slide-right-show",
-//           "slide-left-show",
-//           "slide-down-show"
-//         );
-//         entry.target.classList.remove(
-//           "slide-up-hidden",
-//           "slide-right-hidden",
-//           "slide-left-hidden",
-//           "slide-down-hidden"
-//         );
-//         setAnimationPlayed(entry.target);
-//       } else {
-//         entry.target.classList.remove(
-//           "slide-up-show",
-//           "slide-right-show",
-//           "slide-left-show",
-//           "slide-down-show"
-//         );
-//       }
-//     });
-//   },
-//   { threshold: 0.2 }
-// );
-
-// const hiddenElements = document.querySelectorAll(
-//   ".slide-up-hidden, .slide-right-hidden, .slide-left-hidden, .slide-down-hidden"
-// );
-// hiddenElements.forEach((el) => {
-//   el.classList.add("hidden");
-//   observer.observe(el);
-// });
-
-// function hasAnimationPlayed(element) {
-//   return element.getAttribute("data-animation-played") === "true";
-// }
-
-// function setAnimationPlayed(element) {
-//   element.setAttribute("data-animation-played", "true");
-// }
-
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry, index) => {
     console.log(entry);
