cc625966540c85a3c135d00b592f28a6c2f13224
hansanad <hansana123dasanayake@gmail.com>
2023-05-17 11:05:20 +0530

    Animations on scroll 50% on the page done

--- public/js/main.js
+++ public/js/main.js
@@ -9,21 +9,49 @@ window.onscroll = function () {
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     header.classList.add("nav-container-shrink-sm", "nav-container-shrink");
-  
   } else {
     header.classList.remove("nav-container-shrink-sm", "nav-container-shrink");
   }
-
 }
 
 // OnScroll Loading Effect //
 
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry, index) => {
+//     console.log(entry);
+//     if (entry.isIntersecting) {
+//       setTimeout(() => {
+//         entry.target.classList.add(
+//           "slide-up-show",
+//           "slide-right-show",
+//           "slide-left-show",
+//           "slide-down-show"
+//         );
+//       }, 200 * index);
+//     } else {
+//       entry.target.classList.remove(
+//         "slide-up-show",
+//         "slide-right-show",
+//         "slide-left-show",
+//         "slide-down-show"
+//       );
+//     }
+//   });
+// });
+
+// const hiddenElements = document.querySelectorAll(
+//   ".slide-up-hidden, .slide-right-hidden, .slide-left-hidden, .slide-down-hidden"
+// );
+// hiddenElements.forEach((el) => observer.observe(el));
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry, index) => {
     console.log(entry);
-    if (entry.isIntersecting) {
+    if (entry.isIntersecting && !hasAnimationPlayed(entry.target)) {
       setTimeout(() => {
         entry.target.classList.add('slide-up-show', 'slide-right-show', 'slide-left-show');
+        entry.target.classList.remove('slide-up-hidden', 'slide-right-hidden', 'slide-left-hidden');
+        setAnimationPlayed(entry.target);
       }, 200 * index);
     } else {
       entry.target.classList.remove('slide-up-show', 'slide-right-show', 'slide-left-show');
@@ -32,6 +60,15 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 const hiddenElements = document.querySelectorAll('.slide-up-hidden, .slide-right-hidden, .slide-left-hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+hiddenElements.forEach((el) => {
+  el.classList.add('hidden'); // Add a common class 'hidden' to initially hide the elements
+  observer.observe(el);
+});
 
+function hasAnimationPlayed(element) {
+  return element.getAttribute('data-animation-played') === 'true';
+}
 
+function setAnimationPlayed(element) {
+  element.setAttribute('data-animation-played', 'true');
+}
