a84068115f77a0a699c72e0bd080e968171b526f
hansanad <hansana123dasanayake@gmail.com>
2023-05-25 21:07:47 +0530

    Navbar .active on click feature added

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,24 @@
+// Navbar Active Class Tracker
+
+const pageUrl = window.location.pathname;
+const navbarLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+
+for (var i = 0; i < navbarLinks.length; i++) {
+
+  var link = navbarLinks[i];
+
+  var href = link.getAttribute('href');
+
+  link.classList.remove('active');
+
+  if (pageUrl === href) {
+    link.classList.add('active');
+  }
+}
+
+
+
 // Sticky Glass Menu Styles //
 
 const header = document.querySelector("header");
