104091406193ab3b1523847cf8647f4be91c550c
hansanad <hansana123dasanayake@gmail.com>
2023-05-29 21:55:44 +0530

    Newsletter Subscription & Status Pages Added

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const ejs = require('ejs');
+const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
@@ -8,77 +8,93 @@ require("dotenv").config();
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res)=> {
-    res.render("home");
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get("/menu", (req,res) => {
-    res.render("menu");
+app.get("/menu", (req, res) => {
+  res.render("menu");
 });
 
 app.get("/about", (req, res) => {
-    res.render("about");
+  res.render("about");
 });
 
 app.get("/contact", (req, res) => {
-    res.render("contact");
+  res.render("contact");
 });
 
 app.get("/faq", (req, res) => {
-    res.render("faq");
+  res.render("faq");
 });
-
 
 // Mailchimp Newsletter Submission
 
 app.post("/subscribe", (req, res) => {
-    console.log("Status Code : " + res.statusCode);
-  
-    const email = req.body.email;
-  
-    var data = {
-      members: [
-        {
-          email_address: email,
-          status: "subscribed",
-        },
-      ],
-    };
-  
-    const jsonData = JSON.stringify(data);
-  
-    const url = `https://us11.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_LIST_ID}`;
-  
-    const options = {
-      method: "POST",
-      auth: `hansana:${process.env.MAILCHIMP_API_KEY}`,
-    };
-  
-    const request = https.request(url, options, (response) => {
-      response.on("data", function (data) {
-        console.log(JSON.parse(data));
-      });
-  
-    //   if (response.statusCode === 200) {
-    //       res.send("Submission Successful!");
-    //   } else {
-    //       res.send("Something went wrong!");
-    //   }
+  console.log("Status Code : " + res.statusCode);
 
+  const email = req.body.email;
+
+  var Userdata = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(Userdata);
+
+  const url = `https://us11.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_LIST_ID}`;
+
+  const options = {
+    method: "POST",
+    auth: `hansana:${process.env.MAILCHIMP_API_KEY}`,
+  };
+
+  const request = https.request(url, options, (response) => {
+    response.on("data", function (data) {
+
+      // console.log(JSON.parse(data);
+
+      if (response.statusCode === 200) {
+        res.render("newsletter-status", {
+          title: "Hooray! You have successfully subscribed.",
+          heading: "Subscription Completed!",
+          message: "You have successfully subscribed to our email list.",
+          status_icon: "success-tick",
+        });
+      } else {
+        res.render("newsletter-status", {
+          title: "Oops! Something Went wrong.",
+          heading: "Subscription Failed!",
+          message: `ERROR ${res.statusCode} : An error occurred while submitting.`,
+          status_icon: "failure-cross",
+        });
+      }
 
     });
-  
-    request.write(jsonData);
-    request.end();
-  
   });
-  
 
+  request.write(jsonData);
+  request.end();
+});
+
+app.get("/success", (req, res) => {
+  // res.render("newsletter-status", {
+  //   title: "Hooray! You have successfully subscribed.",
+  //   heading: "Subscription Completed!",
+  //   message:
+  //     "You have successfully subscribed to our email list.",
+  //   status_icon: "success-tick"
+  // });
+});
 
 app.listen(process.env.PORT || 3000, (req, res) => {
-    console.log("Cafesy is up & Running smoothly on locahost.")
+  console.log("Cafesy is up & Running smoothly on locahost.");
 });
