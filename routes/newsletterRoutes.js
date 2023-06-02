c7cea9b2b78f7e4f66c8817bf9a5b0c32d295652
hansanad <hansana123dasanayake@gmail.com>
2023-06-02 12:15:16 +0530

    Express Routes Seperated

---
+++ routes/newsletterRoutes.js
@@ -0,0 +1,110 @@
+const express = require("express");
+const router = express.Router();
+
+// Mailchimp Newsletter Submission
+
+function processSubscription(
+  status,
+  successTitle,
+  successHeading,
+  successMessage,
+  failureTitle,
+  failureHeading,
+  res,
+  req
+) {
+  const email = req.body.email;
+
+  const Userdata = {
+    members: [
+      {
+        email_address: email,
+        status: status,
+      },
+    ],
+    update_existing: true,
+  };
+
+  const jsonData = JSON.stringify(Userdata);
+  const url = `https://us11.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_LIST_ID}`;
+
+  const options = {
+    method: "POST",
+    auth: `hansana:${process.env.MAILCHIMP_API_KEY}`,
+  };
+
+  const request = https.request(url, options, (response) => {
+    response.on("data", function (data) {
+      // console.log(JSON.parse(data));
+
+      if (response.statusCode === 200) {
+        res.render("newsletter-status", {
+          title: successTitle,
+          heading: successHeading,
+          message: successMessage,
+          status_icon: "success-tick",
+        });
+      } else {
+        res.render("newsletter-status", {
+          title: failureTitle,
+          heading: failureHeading,
+          message: `ERROR ${response.statusCode} : An error occurred.`,
+          status_icon: "failure-cross",
+        });
+      }
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+}
+
+router.get("/newsletter", (req, res) => {
+  res.render("newsletter", {
+    title: "Stay Tuned!",
+    heading: "Subscribe to Newsletter!",
+    btntext: "Let's Go!",
+    text: "Wanna Know About Latest Offers & Events? <br>Then Subscribe to our email list and stay updated!",
+    form_action: "/subscribe-confirmation",
+  });
+});
+
+router.get("/unsubscribe", (req, res) => {
+  res.render("newsletter", {
+    title: "You will be missed!",
+    heading: "You will be missed!",
+    btntext: "Unsubscribe me!",
+    text: "By submitting this form you will be removed from our email list and <br> You will no longer able to hear about our events & offers.",
+    form_action: "/unsubscribe-confirmation",
+  });
+});
+
+// Mailchimp Newsletter Submission - Redirect pages
+
+router.post("/subscribe-confirmation", (req, res) => {
+  processSubscription(
+    "subscribed",
+    "Hooray! You have successfully subscribed.",
+    "Subscribed!",
+    "You have successfully subscribed to our email list.",
+    "Oops! Something went wrong.",
+    "Subscription Failed!",
+    res,
+    req
+  );
+});
+
+router.post("/unsubscribe-confirmation", (req, res) => {
+  processSubscription(
+    "unsubscribed",
+    "Bye Bye! You have successfully unsubscribed.",
+    "Unsubscribed!",
+    "You have successfully unsubscribed from our email list.",
+    "Oops! Something went wrong.",
+    "Unsubscription Failed!",
+    res,
+    req
+  );
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 
-require("dotenv").config();
+const contactRoutes = require("./routes/contactRoutes");
+const newsletterRoutes = require("./routes/newsletterRoutes");
 
+require("dotenv").config();
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -24,119 +27,14 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-
-app.get("/contact", (req, res) => {
-  res.render("contact");
-});
-
+app.use("/contact", contactRoutes);
 
 app.get("/faq", (req, res) => {
   res.render("faq");
 });
 
+app.use(newsletterRoutes);
 
-
-// Mailchimp Newsletter Submission
-
-function processSubscription(status, successTitle, successHeading ,successMessage, failureTitle, failureHeading, res, req
-) {
-  const email = req.body.email;
-
-  const Userdata = {
-    members: [
-      {
-        email_address: email,
-        status: status,
-      },
-    ],
-    update_existing: true,
-  };
-
-  const jsonData = JSON.stringify(Userdata);
-  const url = `https://us11.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_LIST_ID}`;
-
-  const options = {
-    method: "POST",
-    auth: `hansana:${process.env.MAILCHIMP_API_KEY}`,
-  };
-
-  const request = https.request(url, options, (response) => {
-    response.on("data", function (data) {
-      // console.log(JSON.parse(data));
-
-      if (response.statusCode === 200) {
-        res.render("newsletter-status", {
-          title: successTitle,
-          heading: successHeading,
-          message: successMessage,
-          status_icon: "success-tick",
-        });
-      } else {
-        res.render("newsletter-status", {
-          title: failureTitle,
-          heading: failureHeading,
-          message: `ERROR ${response.statusCode} : An error occurred.`,
-          status_icon: "failure-cross",
-        });
-      }
-    });
-  });
-
-  request.write(jsonData);
-  request.end();
-}
-
-app.get("/newsletter", (req, res) => {
-  res.render("newsletter", {
-    title: "Stay Tuned!",
-    heading: "Subscribe to Newsletter!",
-    btntext: "Let's Go!",
-    text: "Wanna Know About Latest Offers & Events? <br>Then Subscribe to our email list and stay updated!",
-    form_action: "/subscribe-confirmation"
-  });
-});
-
-app.get("/unsubscribe", (req, res) => {
-  res.render("newsletter", {
-    title: "You will be missed!",
-    heading: "You will be missed!",
-    btntext: "Unsubscribe me!",
-    text: "By submitting this form you will be removed from our email list and <br> You will no longer able to hear about our events & offers.",
-    form_action: "/unsubscribe-confirmation"
-  });
-
-});
-
-// Mailchimp Newsletter Submission - Redirect pages
-
-app.post("/subscribe-confirmation", (req, res) => {
-
-  processSubscription(
-    "subscribed",
-    "Hooray! You have successfully subscribed.",
-    "Subscribed!",
-    "You have successfully subscribed to our email list.",
-    "Oops! Something went wrong.",
-    "Subscription Failed!",
-    res, req
-  );
-});
-
-app.post("/unsubscribe-confirmation", (req, res) => {
-
-  processSubscription(
-    "unsubscribed",
-    "Bye Bye! You have successfully unsubscribed.",
-    "Unsubscribed!",
-    "You have successfully unsubscribed from our email list.",
-    "Oops! Something went wrong.",
-    "Unsubscription Failed!",
-    res, req
-  );
-
-});
-
-
-app.listen(process.env.PORT || 3000, (req, res) => {
+app.listen(3000 || process.env.PORT, (req, res) => {
   console.log("Cafesy is up & Running smoothly on locahost.");
 });
